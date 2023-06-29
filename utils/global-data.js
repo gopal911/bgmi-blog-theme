fe0eee65225fa13aef00a7ad7d1375ea61647b78
gopal911 <82576386+gopal911@users.noreply.github.com>
2023-06-29 18:57:21 +0530

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'RCB ESPORTS';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Join Battle';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
